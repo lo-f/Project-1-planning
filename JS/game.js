10029a238b171fbb8a0792d8505641fc9de391fa
lo-franky <lo.frankyl@gmail.com>
2024-06-24 21:29:44 -0500

    added draw snake functions and modified canvas border

--- JS/game.js
+++ JS/game.js
@@ -2,7 +2,7 @@
 
 const canvas = document.getElementById("gameArea")
 const ctx = gameArea.getContext("2d");
-ctx.lineWidth = 20;
+ctx.lineWidth = 10;
 
 /* ----------------------------- STATE VARIABLE ----------------------------- */
 
@@ -16,15 +16,41 @@ function drawApple () {
     let apple_y;
     ctx.fillStyle = 'red';
     ctx.strokeStyle = 'red';
-    ctx.fillRect(apple_x, apple_y , 10, 10);
-    ctx.strokeRect(apple_x, apple_y, 10, 10);
+    // ctx.fillRect(apple_x, apple_y , 10, 10);
+    // ctx.strokeRect(apple_x, apple_y, 10, 10);
+    ctx.fillRect(0, 0 , 10, 10);
+    ctx.strokeRect(0, 0, 10, 10);
+}
+drawApple ();
+
+
+// Draw Snake------------
+let snake = [
+    {x:300, y:200},
+    {x:290, y:200},
+    {x:280, y:200},
+    {x:270, y:200},
+    {x:260, y:200},
+    {x:250, y:200},
+    {x:240, y:200},
+]
+
+function drawSnake() {
+    snake.forEach(drawSnakeBody);
 }
 
-// drawApple ();
+function drawSnakeBody (snakeBody) {
+    ctx.fillStyle = 'black';
+    ctx.strokeStyle = 'black';
+    ctx.fillRect(snakeBody.x, snakeBody.y, 5, 5);
+    ctx.strokeRect(snakeBody.x, snakeBody.y, 5, 5);
+    // ctx.fillRect(300, 200, 1, -100);
+    // ctx.strokeRect(300, 200, 1, 1);
+}
+
+drawSnake()
 
 
-
-console.dir(drawApple)
 /* ----------------------------- CACHED ELEMENTS ---------------------------- */
 
 
